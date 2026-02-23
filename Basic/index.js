a32e8bddc512f2dbe86e37702869c204dd08cbe8
monirul8888 <monirul429588@gmail.com>
2026-02-24 01:21:13 +0600

    Added Local Storage

---
+++ Basic/index.js
@@ -0,0 +1,13 @@
+
+const nameButton = () =>{
+    const name =document.getElementById("name").value;
+    console.log(name);
+    const id = document.getElementById("id").value;
+    console.log(id);
+
+    const newObj = {name, id};
+    console.log(newObj);
+
+    
+
+}
