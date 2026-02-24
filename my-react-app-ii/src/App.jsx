e3f31c88d079b8d53dad4930b173e49581929e0e
monirul8888 <monirul429588@gmail.com>
2026-02-24 20:23:01 +0600

    added button

--- my-react-app-ii/src/App.jsx
+++ my-react-app-ii/src/App.jsx
@@ -1,15 +1,28 @@
 import './App.css'
 
-function App() {
+function App() 
+{
  function handleClick(){
   alert("I am Clicked")
  }
+
+
+ function handleClick3(){
+  alert("I am Clicked 3");
+ }
+
   return (
     <>
       
       <h1>Vite + React</h1>
       {/* <button onClick = "handleClick()">Click Me</button> */}
       <button onClick={handleClick}>Click Me</button>
+
+      <button onClick={function(){
+        alert("Clciked 2");
+      }}>Click Me 2</button>
+
+      <button onClick={handleClick3}>Click 3</button>
     
      
     </>
