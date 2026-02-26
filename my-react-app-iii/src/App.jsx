20b2eab651c9f7e533b5ef94dd1d462e2eb8160f
monirul8888 <monirul429588@gmail.com>
2026-02-26 23:35:18 +0600

    added fetch

--- my-react-app-iii/src/App.jsx
+++ my-react-app-iii/src/App.jsx
@@ -1,18 +1,21 @@
-import User from './user';
-import './App.css'
+import User from "./user";
+import "./App.css";
+import { Suspense } from "react";
+
+const fetchUser = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (res) => res.json()
+)
 
 function App() {
-  
-
   return (
     <>
-      
       <h1>Vite + React</h1>
-      <User></User>
-     
-    
+      
+      <Suspense fallback = {"Loading ...."}>
+        <User fetchUser = {fetchUser}></User>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- my-react-app-iii/src/user.jsx
+++ my-react-app-iii/src/user.jsx
@@ -1,7 +1,11 @@
-export default function User(){
+import {use} from "react"
+export default function User({fetchUser}){
+
+    const user = use(fetchUser);
+    console.log(user);
     return(
         <div className="card">
-            <h3>User</h3>
+            <h3>User = {user.length}</h3>
         </div>
     )
 }
