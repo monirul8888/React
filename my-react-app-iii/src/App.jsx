ac0f1140fd9832513d2c95ddfaaea3c4cb1a4e5e
monirul8888 <monirul429588@gmail.com>
2026-02-26 23:52:55 +0600

    Fetch Friends

--- my-react-app-iii/src/App.jsx
+++ my-react-app-iii/src/App.jsx
@@ -4,23 +4,27 @@ import { Suspense } from "react";
 import Friends from "./Friends";
 
 const fetchUser = fetch("https://jsonplaceholder.typicode.com/users").then(
-  (res) => res.json()
-)
+  (res) => res.json(),
+);
+
+const fetchFriends = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
+
 
 function App() {
+  const friendsPromise = fetchFriends();
   return (
     <>
       <h1>Vite + React</h1>
-      
-      <Suspense fallback = {"Loading ...."}>
-        <User fetchUser = {fetchUser}></User>
+
+      <Suspense fallback={"Loading ...."}>
+        <User fetchUser={fetchUser}></User>
       </Suspense>
 
-      <Suspense fallback = {<h3>
-        Friends are Comming for Treat
-      </h3>
-      }>
-        <Friends></Friends>
+      <Suspense fallback={<h3>Friends are Comming for Treat</h3>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
       </Suspense>
     </>
   );
--- my-react-app-iii/src/Friends.jsx
+++ my-react-app-iii/src/Friends.jsx
@@ -1,7 +1,12 @@
-export default function Friends(){
+import { use } from "react"
+export default function Friends({friendsPromise}){
+
+     const friends = use(friendsPromise);
+     
     return(
+       
         <div className="card">
-            <h2>Friends</h2>
+            <h2>Friends = {friends.length}</h2>
         </div>
     )
 }
