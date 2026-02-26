f2de31ca070a4ead2ee7af9d143e9aeb4cc5acac
monirul8888 <monirul429588@gmail.com>
2026-02-26 22:58:49 +0600

    Working on suspense

--- my-react-app-ii/src/App.jsx
+++ my-react-app-ii/src/App.jsx
@@ -1,33 +1,13 @@
 import './App.css'
-import  Batsman from "./Batman.jsx";
 
 function App() 
 {
- function handleClick(){
-  alert("I am Clicked")
- }
-
-
- function handleClick3(){
-  alert("I am Clicked 3");
- }
-
-const  handleClick5=(num)=>{
-  const newNumber = num+2;
-  alert(newNumber);
- }
-
+ 
   return (
-    <>
-      
-      <h1>Vite + React</h1>
-
-      <Batsman name="Monirul"></Batsman>
-
-
-     
+    <>  
     
-     
+      <h1>Vite + React</h1>
+      
     </>
   )
 }
