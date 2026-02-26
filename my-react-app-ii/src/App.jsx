7b5173b3d6c8a8ab83e332935f55c8fa4062d9fd
monirul8888 <monirul429588@gmail.com>
2026-02-26 23:01:07 +0600

    Working on suspense

--- my-react-app-ii/src/App.jsx
+++ my-react-app-ii/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import Users  from './user'
 
 function App() 
 {
  
   return (
     <>  
-    
+
       <h1>Vite + React</h1>
+      <user></user>
       
     </>
   )
--- my-react-app-ii/src/user.jsx
+++ my-react-app-ii/src/user.jsx
@@ -1,9 +1,9 @@
 export default function Users(){
 
-    return{
+    return(
         <div>
         <h3>Users</h3>
         </div>
-    }
+    )
 
 }
