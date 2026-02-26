a9ea87a6b5c7c7361cc6e6ec11aff3527c475192
monirul8888 <monirul429588@gmail.com>
2026-02-27 02:50:26 +0600

    added friends [name, email, phone]

--- my-react-app-iii/src/user.jsx
+++ my-react-app-iii/src/user.jsx
@@ -2,10 +2,11 @@ import {use} from "react"
 export default function User({fetchUser}){
 
     const user = use(fetchUser);
-    console.log(user);
+   
     return(
         <div className="card">
             <h3>User = {user.length}</h3>
+            
         </div>
     )
 }
